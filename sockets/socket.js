273b2f46115721e15e1663addd7787f23f6fb9b7
ArthourVaSa <arjavasaes21@gmail.com>
2022-12-05 11:59:37 -0500

    Finishing the back end code

--- sockets/socket.js
+++ sockets/socket.js
@@ -26,4 +26,19 @@ io.on('connection', client => {
         client.broadcast.emit('nuevo-mensaje', payload);
     });
 
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
 });
